34693ee5f6a7393f8e7211e65600a23237a06a43
UndMund <zavadskiy.nazar@mail.ru>
2023-05-26 19:36:51 +0300

    jdbc xml config

--- src/main/java/org/example/config/ApplicationConfiguration.java
+++ src/main/java/org/example/config/ApplicationConfiguration.java
@@ -2,10 +2,12 @@ package org.example.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-@PropertySource("classpath:application.properties")
 @ComponentScan(basePackages = "org.example")
+@PropertySource("classpath:application.properties")
+@Import({ProdConfiguration.class, TestConfiguration.class})
 public class ApplicationConfiguration {
 }
